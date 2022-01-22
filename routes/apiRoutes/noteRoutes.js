29afd5ecde34a57d1f4bb75fb38b25291808798c
Erik Plachta <eplachta@gmail.com>
2022-01-22 18:03:30 -0500

    Add post and veified building uuid

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,38 +2,14 @@
 const router = require('express').Router();
 
 //-- define what can be imported
-const { findById, setNotes, validateNote } = require('../../lib/notes');
+// const { findById, setNotes, validateNote } = require('../../lib/notes');
+
 // //-- import notes data
-const { notes } = require('../../db/notes');
+const notes = require('../../db/notes.json');
 
-router.get('/note', (req, res) => {
-  let results = notes;
-  // if (req.query) {
-  //   results = filterByQuery(req.query, results);
-  // }
-  
-  res.json("results");
-});
 
-// router.get('/notes/:id', (req, res) => {
-//   const result = findById(req.params.id, notes);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.send(404);
-//   }
-// });
+// router.get('/notes', (req, res) => res.json(notes));
+router.get('/notes', (req, res) => res.json(notes));
 
-// router.post('/notes', (req, res) => {
-//   // set id based on what the next index of the array will be
-//   req.body.id = animals.length.toString();
-
-//   if (!validateNote(req.body)) {
-//     res.status(400).send('The animal is not properly formatted.');
-//   } else {
-//     const note = setNote(req.body, notes);
-//     res.json(note);
-//   }
-// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,48 +1,108 @@
 //------------------------------------------------------------------------------
-//-- IMPORTS
+//-- Setting up express
 
 //-- onboard express.js server
 const express = require('express');
-
-//------------------------------------------------------------------------------
-//-- 
-
 //-- define port
 const PORT = process.env.PORT || 3001;
-
 //-- create instance
 const app = express();
 
+//-- Required to push to a HTML
+const path = require('path');
+
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
 //------------------------------------------------------------------------------
 //-- Route definitions
-
 const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
-
-
 //------------------------------------------------------------------------------
 //-- Ability to use JSON
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //------------------------------------------------------------------------------
 //-- Defining path to public content
-
 app.use(express.static('public'));
-
 
 //------------------------------------------------------------------------------
 //-- Defining Routes
 
-// Use apiRoutes
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
 
-const path = require('path');
-const notes = require('./db/notes.json');
-app.get('/note', (req, res) => res.json(notes));
+//------
+//-- API
+
+//-- getting notes
+app.get('/api/notes', (req, res) => {
+  const notes = require('./db/notes.json');
+  res.json(notes);
+});
+
+//-- setting notes
+app.post('/api/notes', (req, res) => {
+
+  console.info(`${req.method} request received to add a note.`);
+
+
+  //-- Extract payload
+  const { title, body } = req.body;
+  
+  if (title && body) {
+    const newNote = {
+      title,
+      body,
+      note_ID: uuid(),
+    }
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    //-- Testing a response here
+    console.log(response);
+
+    res.json(response);
+  }
+  else {
+    //-- If it receives a bad payload, to client
+    res.json(`Error in posting review: ${JSON.stringify(req.body)}`);
+  }
+
+});
+
+
+// app.use('/api', apiRoutes);
+// // app.use('/', htmlRoutes);
+
+
+app.get('/test', (reg, res) => res.status(200).send("Local response from server.js successful."));
+
+//-- Testing to verify direct routing works, here.
+// const path = require('path');
+
+//-- to get a JSON database
+// const notes = require('./db/notes.json');
+// app.get('/notes', (req, res) => res.json(notes));
+
+
+
+//-------
+//-- HTML 
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+//-- bad path, go to index
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 
 
 //------------------------------------------------------------------------------
